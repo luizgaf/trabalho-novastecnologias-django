a1a000a1ff3318ea1a87d6628869d75443273793
luizgaf <luiz.guilhermeaf21@gmail.com>
2025-05-09 15:19:10 -0300

    estamos fritos

--- ecodashboardproject/ecodashboardproject/asgi.py
+++ ecodashboardproject/ecodashboardproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ecodash project.
+ASGI config for ecodashboardproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecodash.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecodashboardproject.settings')
 
 application = get_asgi_application()
--- ecodashboardproject/ecodashboardproject/wsgi.py
+++ ecodashboardproject/ecodashboardproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ecodash project.
+WSGI config for ecodashboardproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecodash.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecodashboardproject.settings')
 
 application = get_wsgi_application()
